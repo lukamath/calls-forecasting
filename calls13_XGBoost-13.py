10bdd1455c7a3ea950fc76ab1ba9f411efa534ea
lukamath <lukamath@cella1.com>
2023-11-21 14:58:57 +0100

    hyper tuning

--- calls13_XGBoost-13.py
+++ calls13_XGBoost-13.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import xgboost as xgb
 import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
 
 # Step 1: Load the data
 df = pd.read_csv('calls2022-2023.csv', sep=';', parse_dates=['date'], dayfirst=True)
@@ -40,22 +41,39 @@ print(cleaned_df.groupby(['hour', 'weekday', 'month', 'week'])['value'].describe
 print(f"Total number of rows before filtering: {len(df)}")
 print(f"Total number of rows after filtering: {len(cleaned_df)}")
 
-
+# Calculate historical data based on the specified prediction date's weekday, month, and adjacent weeks
 historical_data = cleaned_df[
-    ((cleaned_df['weekday'] == prediction_date.weekday()) & (cleaned_df['month'].between(prediction_date.month - 1, prediction_date.month + 1)))
+    ((cleaned_df['weekday'] == prediction_date.weekday()) & (cleaned_df['month'].between(prediction_date.month - 1, prediction_date.month + 1)) & (cleaned_df['week'].between(prediction_date.isocalendar().week - 1, prediction_date.isocalendar().week + 1)))
 ]
-
 
 # Use the filtered data to train the model
 X_train_filtered, y_train_filtered = cleaned_df[['hour', 'weekday', 'month', 'week']], cleaned_df['value']
 
-# Step 7: Train the XGBoost model using filtered data
-model = xgb.XGBRegressor(objective='reg:squarederror')
-model.fit(X_train_filtered, y_train_filtered)
+# Step 7: Hyperparameter Tuning using GridSearchCV
+param_grid = {
+    'n_estimators': [50, 100, 150],
+    'learning_rate': [0.01, 0.1, 0.2],
+    'max_depth': [3, 5, 7],
+}
+
+grid_search = GridSearchCV(
+    xgb.XGBRegressor(objective='reg:squarederror'),
+    param_grid,
+    cv=3,
+    scoring='neg_mean_squared_error',
+)
+
+grid_search.fit(X_train_filtered, y_train_filtered)
+
+# Print the best hyperparameters
+print("Best Hyperparameters:", grid_search.best_params_)
+
+# Use the best model from GridSearchCV
+best_model = grid_search.best_estimator_
 
 # Step 8: Make predictions on the test set
 X_test, y_test = test[['hour', 'weekday', 'month', 'week']], test['value']
-y_pred = model.predict(X_test)
+y_pred = best_model.predict(X_test)
 
 # Check for NaN values
 if any(pd.isna(y_test)) or any(pd.isna(y_pred)):
@@ -75,15 +93,19 @@ else:
     ax.scatter(historical_data['hour'], historical_data['value'], label='Historical Values', color='blue', marker='o', alpha=0.5)
 
     # Plot predicted values for the user-specified date
-    X_user = pd.DataFrame({'hour': range(9, 25), 'weekday': [prediction_date.weekday()] * 16, 'month': [prediction_date.month] * 16, 'week': [prediction_date.isocalendar().week] * 16})
-    y_user_pred = model.predict(X_user)
+    X_user = pd.DataFrame({'hour': range(9, 25), 'weekday': [prediction_date.weekday()] * 16, 'month': [prediction_date.month] * 16,
+    'week': [prediction_date.isocalendar().week] * 16,
+        })
+
+    y_user_pred = best_model.predict(X_user)
     ax.scatter(X_user['hour'], y_user_pred, label='Predicted Values (User Input)', color='red', marker='x')
 
     ax.set_title('Historical and Predicted Values Plot')
     ax.set_xlabel('Hour')
     ax.set_ylabel('Values')
     ax.set_xticks(range(9, 25))  # Set x-axis ticks for hours from 9 to 24
-    ax.set_yticks(range(0, 900, 50))  # Set x-axis ticks for hours from 9 to 24
+    ax.set_yticks(range(0, 900, 50))  # Set y-axis ticks
+
     ax.legend()
 
     plt.tight_layout()
